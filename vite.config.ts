1c7f7ab8ddd7423adb11fab4c80bd70e0bb4dd9f
Deep Sharma <Deepsharma132006@gmail.com>
2026-09-14 15:17:32 +0530

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vadodaratechinnovations/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
