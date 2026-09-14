616487959331b810d177a6111d356bc1d051970c
Deep Sharma <Deepsharma132006@gmail.com>
2026-09-14 14:51:12 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vadodaratechinnovations",
   plugins: [react()],
   resolve: {
     alias: {
